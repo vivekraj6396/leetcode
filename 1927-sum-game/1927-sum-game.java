4f5cd4e450393b3b7a4c0adbfb335c3982d2ea3b
vivekraj6396 <singhsudipiti@gmail.com>
2026-08-23 09:37:23 +0530

    Time: 10 ms (16.06%), Space: 47.4 MB (10.95%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean sumGame(String num) {
-
         int sum1=0;
         int sum2=0;
         int count1=0;
         int count2=0;
-
         for(int i=0;i<num.length()/2;i++)
         {
             if(num.charAt(i)=='?')
@@ -17,7 +15,6 @@ class Solution {
                 sum1=sum1+(num.charAt(i)-'0');
             }
         }
-
         for(int i=num.length()/2;i<num.length();i++)
         {
             if(num.charAt(i)=='?')
@@ -29,19 +26,15 @@ class Solution {
                 sum2=sum2+(num.charAt(i)-'0');
             }
         }
-
         int q=Math.abs(count1-count2);
-
         if(q%2==1)
         {
             return true;
         }
-
         if(sum1-sum2==9*(count2-count1)/2)
         {
             return false;
         }
-
         return true;
     }
 }
